c28506a0432715ab220fc9b17c57ec226f8dbb90
Yura Komarov <yurajun@mail.ru>
2021-01-22 15:09:42 +0300

    Первоначальные настройки приложения

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,27 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {},
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        allowImportExportEverywhere: false,
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@nuxtjs',
+        'prettier',
+        'prettier/vue',
+        // 'plugin:prettier/recommended',
+        'plugin:nuxt/recommended',
+    ],
+    plugins: ['vue', 'html'],
+    // add your custom rules here
+    rules: {
+        'no-console': ['error', { allow: ['warn', 'error', 'log'] }],
+    },
 }
